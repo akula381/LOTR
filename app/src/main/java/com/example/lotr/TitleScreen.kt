c13837b5cfe84b2445eaf6c4c668af23550b6b66
akula381 <akulanvn381@gmail.com>
2022-12-12 19:16:43 -0700

    Initial commit

---
+++ app/src/main/java/com/example/lotr/TitleScreen.kt
@@ -0,0 +1,18 @@
+package com.example.lotr
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TitleScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_title_screen)
+
+        setOnClickListener()
+    }
+}
+
+private fun setOnClickListener() {
+
+}
